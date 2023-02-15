b322a8b2e508d00b1ca72caee200593a763b26e8
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-02-15 17:07:38 +0000

    restructed pages to accomdate links

---
+++ src/Pages/Checkout.js
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+
+
+const Checkout = () => {
+
+    // const { checkout } = useParams();
+
+
+    return(
+       <div className="checkout">
+        
+        <div><h3>Shopping Basket</h3></div>
+
+        <h2>Delivery address</h2>
+
+        <h2>Pay below!</h2>
+
+
+
+        <button>Buy now</button>
+
+        </div>
+
+        
+        
+    )
+}
+
+
+
+
+
+
+
+
+
+export default Checkout;
---
+++ src/Pages/Home.js
@@ -0,0 +1,125 @@
+import { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+import '../App.css';
+import Header from "../components/Header"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const customStyles = {
+    content: {
+      top: '40%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      maxWidth: `600px`,
+      backgroundColor: `rgb(201, 200, 205)`,
+      borderRadius: `20px`
+    },
+  };
+  
+  Modal.setAppElement(`#root`);
+
+
+
+
+const Home = () => {
+
+    let subtitle;
+    const [catData, setCatData] = useState([]);
+    const [error, setError] = useState(null);
+    const [openCatInfo, setOpenCatInfo] = useState(false);
+    const [selectedCatInfo, setSelectedCatInfo] = useState();
+  
+    useEffect(() => {
+      const fetchData = async () => {
+  
+        try {
+          setError(null);
+          const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+  
+          if (!response.ok) {
+            throw new Error(response.status);
+          }
+  
+          const data = await response.json();
+          setCatData(data.splice(0, 9));
+          console.log(data)
+  
+        } catch (error) {
+          setError("We're not Puurrrrfect-Something went wrong...");
+        }
+      }
+      fetchData();
+    }, [])
+  
+  
+    const openModal = () => {
+      setOpenCatInfo(true);
+    }
+  
+    const afterOpenModal = () => {
+      subtitle.style.color = '#f00';
+    }
+  
+    const closeModal = () => {
+      setOpenCatInfo(false);
+    }
+  
+    return (
+        <div className="App">
+      <Header />
+      {error!== null && <h3>{error}</h3>}
+      <div id="catBox">
+        <div id="catGrid">
+          {catData.map((cat, index) => {
+            return (
+              <button key={index}
+                onClick={() => {
+                  setSelectedCatInfo(cat)
+                  openModal()
+                  console.log("click")
+                }}>
+                <img id="catImages" src={cat.url} alt="catImage" />
+              </button>
+            )
+          })}
+        </div>
+      </div>
+      <div>
+        {selectedCatInfo &&
+          <Modal
+            isOpen={openCatInfo}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <div id="modalXFlexBox">
+              <button id="closeModalBtn" onClick={closeModal}>&times;</button>
+            </div>
+
+            <div id="ModalTextInfo">
+              <h2>Am I your Puuurfect Pet?</h2>
+              {/* <h2> {selectedCatInfo["Species Name"]}</h2>
+              <h5> {selectedCatInfo["Scientific Name"]}</h5>
+              <p dangerouslySetInnerHTML={{ __html: selectedCatInfo["Location"] }} />
+              <p dangerouslySetInnerHTML={{ __html: selectedCatInfo["Population"] }} />
+              <p dangerouslySetInnerHTML={{ __html: selectedCatInfo["Color"] }} /> */}
+            </div>
+
+            {/* <img id="ModalImg"
+              src={selectedCatInfo["Species Illustration Photo"].src}
+              alt={selectedCatInfo["Species Illustration Photo"].alt}
+              title={selectedCatInfo["Species Illustration Photo"].title}
+            /> */}
+          </Modal>
+        }
+      </div>
+    </div>
+    )
+
+
+
+}
+    export default Home;
