8d109178e7d1386ca18ece5818d5c1564ee154db
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-02-16 11:45:36 +0000

    checkoutpage Updated

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -10,25 +10,26 @@ const Checkout = () => {
     // const { checkout } = useParams();
 
     return (
-       
+
         <div id='checkoutBox'> <div id="checkoutGrid">
 
-            <div className='Titles'><h1 id="shoppingtitle">Shopping Basket:</h1>
-            <h2 id='paybelow'>Pay below:</h2>
-            <h2 id='delivery'>Delivery address:</h2>
+            <div className='Titles'><h1 id="shoppingtitle">Shopping Basket</h1> <div id='items'><p>items</p></div>
+                <h2 id='paybelow'>Pay below</h2>
+
+                <div className='payimages'> <img id="payImages" src = ".src\images\Paypal-logo.png" alt="visaImage" />
+                <img id="payImages" src = ".src/images/VISA-symbol.jpg" alt="visaImage" /></div>
+
+                <div>  <h2 id='delivery'>Delivery address</h2> 
+                 </div><input className='deliveryDetails' placeholder='Fill in details..'></input>
             </div>
 
-            <div id='BuybuttonBox'> <div id='texts'> <button className='BuyButton'>Buy now</button> <p id='ordersummary'>Order total</p></div> </div>
-            
-
-            
+            <div id='BuybuttonBox'> <div id='texts'> <button className='BuyButton'>Buy now</button> <p id='ordersummary'>Order total:</p></div>
+             </div>
 
 
-           
-
-          </div>
         </div>
-        
+        </div>
+
     )
 }
 
