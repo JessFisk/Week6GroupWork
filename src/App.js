f7c799be829da938d961d858a1521bdb3db57864
Jessica Fisk <fisk.jess@gmail.com>
2023-02-16 18:13:06 +0000

    Created state in App for basket and used props to pass data to home and checkout, Now hav the ability to add to basket

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Header from "./components/Header"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Checkout from "./Pages/Checkout";
+import { useState } from "react";
 
 
 const App = () => {
-
+  const [basket, setBasket] = useState ([])
+  
   return (
     <BrowserRouter>
-
+        {JSON.stringify(basket)}
       <Header> </Header>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Checkout" element={<Checkout />}></Route>
+        <Route path="/" element={<Home basket={basket} updateBasket={setBasket} />}></Route>
+        <Route path="/Checkout" element={<Checkout basket={basket} />}></Route>
       </Routes>
 
-
     </BrowserRouter>
-
 
   );
 }
